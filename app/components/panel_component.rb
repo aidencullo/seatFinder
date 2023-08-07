95398524a494c8163638145bb0bb612aa332b02b
Aiden Cullo <aidencullo@gmail.com>
2023-08-07 20:54:00 -0300

    basic dashboard

---
+++ app/components/panel_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PanelComponent < ViewComponent::Base
+  def initialize(title:)
+    @title = title
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_223237) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_170927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "documents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +40,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_223237) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "paragraphs", force: :cascade do |t|
+    t.bigint "section_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_paragraphs_on_section_id"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.bigint "document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["document_id"], name: "index_sections_on_document_id"
+  end
+
   create_table "showings", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
@@ -34,4 +61,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_223237) do
     t.string "movie_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "addresses", "users"
+  add_foreign_key "paragraphs", "sections"
+  add_foreign_key "sections", "documents"
+  add_foreign_key "tasks", "users"
 end
