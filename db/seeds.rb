d3d03a608933cb038225a4f9f35da83697788d71
Aiden Cullo <aidencullo@gmail.com>
2023-08-15 19:10:49 -0300

    added nested attributes to event seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+params = {
+  :venue_id => 1,
+  :company_id => 1,
+  :grid_attributes => {
+    :rows => 2,
+    :cols => 3,
+  },
+}
+
 Company.destroy_all
 Customer.destroy_all
 Venue.destroy_all
@@ -17,7 +26,7 @@ Customer.create
 Venue.create
 Venue.create
 Venue.create
-Event.create(venue_id:1, company_id:1)
-Event.create(venue_id:1, company_id:1)
-Event.create(venue_id:1, company_id:1)
+Event.create(params)
+Event.create(params)
+Event.create(params)
 Ticket.create(event_id:1, customer_id:1)
