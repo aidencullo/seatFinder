a40edb227bc6412a6e0507a61f60c26ae5801784
Aiden Cullo <aidencullo@gmail.com>
2023-08-28 16:19:11 -0300

    extract grid cells to their own partials

--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -1,10 +1,10 @@
 module GridHelper
-  def local_ticket(grid, i, k)
+  def has_ticket(grid, i, k)
     return Ticket.where(event_id: grid.event_id, seat: grid_index(grid,
                                                                   i, k)).exists?
   end
 
-  def local_ticket_path(grid, i, k)
+  def local_ticket(grid, i, k)
     ticket = Ticket.find_by(event_id: grid.event_id, seat:
                                                        grid_index(grid, i, k)) 
     return ticket
